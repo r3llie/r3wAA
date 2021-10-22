8795346cc2fa403a6c51838855c89a0e9c19f5fc
r3llie <ghailstone503@gmail.com>
2021-10-22 13:50:23 -0700

    Added some Javascript

--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
-alert("hello");
+function displayTemperature(response) {
+  let city = document.querySelector("#city-element");
+  city.innerHTML = response.data.name;
+  let temperature = document.querySelector("#number-temp");
+  temperature.innerHTML = Math.round(response.data.main.temp);
+  let description = document.querySelector("#weather-description");
+  description.innerHTML = response.data.weather[0].description;
+  let humidity = document.querySelector("#humidity-number");
+  humidity.innerHTML = response.data.main.humidity;
+  let wind = document.querySelector("#wind-number");
+  wind.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiKey = "6f578b96aa9505bcce148ac22cb85794";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=imperial`;
+
+axios.get(apiUrl).then(displayTemperature);
