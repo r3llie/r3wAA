86901ab04eae6a336db1f12d8947153168086611
r3llie <ghailstone503@gmail.com>
2021-10-22 21:27:30 -0700

    Repeat forecast html & javascript

--- src/app.js
+++ src/app.js
@@ -21,6 +21,33 @@ function formatDate(timestamp) {
 
   return `${day} ${hours}:${minutes}`;
 }
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+            <div class="col-2">
+              <div class="weather-forecast-date">
+                ${day}
+              </div>
+              <img src="http://openweathermap.org/img/wn/01n@2x.png" alt="" width="55"/>
+              <div class="weather-forecast-temp">
+                <span class="weather-forecast-high">
+                  18°
+                </span>
+                <span class="weather-forecast-low">
+                  12°
+                 </span>
+                </div>
+            </div>`;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
 
 function displayTemperature(response) {
   let cityElement = document.querySelector("#city");
@@ -56,6 +83,7 @@ function handleSubmit(event) {
 }
 
 search("New York");
+displayForecast();
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
